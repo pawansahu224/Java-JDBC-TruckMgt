758c94cf72a1b5095f3b21e3a35379a20a903905
Pawan Sahu <ps671568@gmail.com>
2026-01-01 14:01:06 +0530

    Initial commit

---
+++ TruckMgt/src/main/java/org/example/App.java
@@ -0,0 +1,56 @@
+package org.example;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+        TruckService truckService = new TruckService();
+        Truck tata = new Truck("TATA","2019",1200,"rajesh");
+        Truck volvo = new Truck("VOLVO","2009",1290,"Akash");
+        Truck Eicher = new Truck("EICHER","2020",1900,"Harsh");
+
+        // adding data into database
+        System.out.println("Adding data.......");
+        truckService.addTruck(tata);
+        truckService.addTruck(volvo);
+        truckService.addTruck(Eicher);
+
+        // fetch
+        System.out.println("Fetching data by id "+1);
+        Truck truck = truckService.getTruckById(1);
+        System.out.println("Truck data : "+truck);
+
+        // updating truck data
+        System.out.println("Updating  data.......");
+        truck.setDriverName("ramesh");
+        truckService.updateTruck(truck);
+        System.out.println("Updated truck data :"+truckService.getTruckById(1));
+
+        // get all truck data
+        System.out.println("Getting all data.......");
+        List<Truck> allTrucks = truckService.getAllTruck();
+        System.out.println("All trucks in DB :");
+        for(Truck truck1 : allTrucks)
+        {
+            System.out.println(truck1);
+        }
+
+        // delete truck data
+        System.out.println("Deleting data.......");
+        truckService.deleteTruck(2);
+        System.out.println("Data delete by id: "+2);
+
+        System.out.println("Getting all data.......");
+        allTrucks = truckService.getAllTruck();
+        System.out.println("All Trucks after all operations :");
+        System.out.println(allTrucks);
+    }
+
+
+}
---
+++ TruckMgt/src/main/java/org/example/connectionDetails.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connectionDetails {
+    //  public static String LOAD_DRIVER="com.mysql.cj.jdbc.Driver";
+    public static final String URL="jdbc:mysql://localhost:3306/truckmgt";
+    public static final String PASSWORD="Pawan$ahu224089";
+    public static final String USERNAME="root";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
+}
